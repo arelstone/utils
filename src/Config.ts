a0dc45e309eafc6df65db2e26f5c47fce86f3dff
Carsten Daurehøj <arelstone@gmail.com>
2022-03-27 14:55:57 +0200

    ✨ Add config

---
+++ src/Config.ts
@@ -0,0 +1,12 @@
+export class Config {
+  static get(key: string, fallback?: string): string {
+    const value = process.env[key] || fallback;
+    if (value === undefined) {
+      throw new Error(`Please provide env variable for ${key}`);
+    }
+
+    return value;
+  }
+
+  static isDevelopment: boolean = Config.get('NODE_ENV') === 'development'
+}
