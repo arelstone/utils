b74a16ab170e0a8d904e1b5f26dd98a71f123869
Carsten Daurehøj <arelstone@gmail.com>
2022-03-30 08:00:15 +0200

    ➕ @arelstone/eslint-config-react-native-typescript for linting (#2)
    * ➕ @arelstone/eslint-config-react-native-typescript for linting
    
    * 🎨 Add files.insertFinalNewline
    
    * 👷 Update pull-request workflow

--- src/Config.ts
+++ src/Config.ts
@@ -1,12 +1,15 @@
 export class Config {
-  static get (key: string, fallback?: string): string {
-    const value = process.env[key] || fallback
-    if (value === undefined) {
-      throw new Error(`Please provide env variable for ${key}`)
+    static get (key: string, fallback?: string): string {
+        const value = process.env[key] || fallback;
+
+        if (value === undefined) {
+            throw new Error(`Please provide env variable for ${key}`);
+        }
+
+        return value;
     }
 
-    return value
-  }
-
-  static isDevelopment: boolean = Config.get('NODE_ENV') === 'development'
+    static isDevelopment: boolean = Config.get('NODE_ENV') === 'development';
+    
+    static env: string = Config.get('NODE_ENV');
 }
