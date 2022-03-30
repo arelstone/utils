71ca1e48c66c8c21f8268976d8968f94c6eb7dda
Carsten Daurehøj <arelstone@gmail.com>
2022-03-30 06:53:55 +0200

    ➕ Add linting config

--- src/Config.ts
+++ src/Config.ts
@@ -1,11 +1,11 @@
 export class Config {
-  static get(key: string, fallback?: string): string {
-    const value = process.env[key] || fallback;
+  static get (key: string, fallback?: string): string {
+    const value = process.env[key] || fallback
     if (value === undefined) {
-      throw new Error(`Please provide env variable for ${key}`);
+      throw new Error(`Please provide env variable for ${key}`)
     }
 
-    return value;
+    return value
   }
 
   static isDevelopment: boolean = Config.get('NODE_ENV') === 'development'
